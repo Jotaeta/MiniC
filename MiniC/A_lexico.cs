53135dbb4a89e6a8ce3ad17b8a71df79108c204e
Allan Davila <allan.davila@hotmail.com>
2020-08-18 10:27:37 -0600

    Update A_lexico.cs
    Agregados los regex de cada token, se debe de completar dos regex indicados.

--- MiniC/A_lexico.cs
+++ MiniC/A_lexico.cs
@@ -5,8 +5,21 @@ using System.Text.RegularExpressions;
 using System.IO;
 namespace MiniC
 {
-    class A_lexico
+    public class A_lexico
     {
-        
+        public static Regex regexPalabrasReservadas = new Regex(@"(void|int|double|bool|string|class|const|interface|null|this|for|while|foreach|if|else|return|break|New|NewArray|Console|WriteLine)");
+        public static Regex regexIdentificadores = new Regex(@"[a-zA-z]+(\w)*");
+        //Falta completar
+        public static Regex regexComentarioLinea = new Regex(@"\/\/.*(?!\*\/)\n");
+        public static Regex regexComentariosMultipleLine = new Regex(@"\/\*.*\*\/");
+        public static Regex regexBool = new Regex(@"(true|false)");
+        public static Regex regexDigitos = new Regex(@"\d*");
+        public static Regex regexHexadecimal = new Regex(@"0[Xx][a-fA-F0-9+]+");
+        public static Regex regexDouble = new Regex(@"\d+\.\d*");
+        public static Regex regexDoubleExponencial = new Regex(@"\d+\.E\+\d+");
+        //Falta completar
+        public static Regex regexString = new Regex("\"([^\"]*)\"");
+        public static Regex regexOperadores = new Regex(@"(\+|\-|\*|\/|\%|\<|\<\=|\>|\>\=|\=|\=\=|\!\=|\&\&|\|\||\!|\;|\,|\.|\[|\]|\(|\)|\{|\}|\[\]|\(\)|\{\})");
+        public static Regex regexSimbolosPermitidos = new Regex(@"(\+|\-|\*|\/|\%|\<|\<\=|\>|\>\=|\=|\=\=|\!\=|\&\&|\|\||\!|\;|\,|\.|\[|\]|\(|\)|\{|\}|\[\]|\(\)|\{\}|[A-Za-z0-9])");
     }
 }
